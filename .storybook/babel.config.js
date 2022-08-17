475ff3e4faf9395e65b5f153952cb7282a8a87cb
Sam Magura <srmagura@gmail.com>
2022-08-17 15:22:16 -0400

    Simplify

--- .storybook/babel.config.js
+++ .storybook/babel.config.js
@@ -1,14 +1,12 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    "@babel/preset-env",
     [
-      '@babel/preset-react',
+      "@babel/preset-react",
       {
-        runtime: 'automatic',
-        importSource: '@emotion/react',
+        runtime: "automatic",
       },
     ],
-    '@babel/preset-typescript',
+    "@babel/preset-typescript",
   ],
-  plugins: [['@babel/plugin-proposal-decorators', { version: '2021-12' }]],
 };
