843aa2fbc265138d353dae961914a859c041ac81
Sam Magura <srmagura@gmail.com>
2022-08-17 15:34:48 -0400

    typo

--- src/Button.stories.tsx
+++ src/Button.stories.tsx
@@ -3,7 +3,7 @@ export default {
 };
 
 class PortalMaterial {
-  #color1!: any; // It works if the explanation point is removed
+  #color1!: any; // It works if the exclamation point is removed
 
   set color1(v: any) {
     this.#color1 = v;
